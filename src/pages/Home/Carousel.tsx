801865097549a1c27006bedc7bae3d274604634f
marlonchiu <Knight1898@outlook.com>
2021-11-20 20:02:05 +0800

    9-5 轮播图 下

--- src/pages/Home/Carousel.tsx
+++ src/pages/Home/Carousel.tsx
@@ -4,22 +4,25 @@
  * @Author: jdzhao@iflytek.com
  * @Date: 2021-11-20 17:53:35
  * @LastEditors: jdzhao@iflytek.com
- * @LastEditTime: 2021-11-20 18:50:02
+ * @LastEditTime: 2021-11-20 20:00:25
  */
 import React from 'react';
 import SnapCarousel, {
+  Pagination,
+  ParallaxImage,
   AdditionalParallaxProps,
 } from 'react-native-snap-carousel';
-import {viewportWidth, viewportHeight, wp, hp} from '@/utils/index';
-import {StyleSheet, Image} from 'react-native';
+import {viewportWidth, wp, hp} from '@/utils/index';
+import {View, StyleSheet} from 'react-native';
 
 // 屏幕宽度
 const sliderWidth = viewportWidth;
 // 图片宽度
-const sideWidth = wp(90);
-const sideHeight = hp(26);
+const slideWidth = wp(90);
+const slideHeight = hp(26);
+const itemHorizontalMargin = wp(2);
 // 轮播图每个item 宽度
-const itemWidth = sideWidth + wp(2) * 2;
+const itemWidth = slideWidth + itemHorizontalMargin * 2;
 
 const data = [
   'https://t7.baidu.com/it/u=1819248061,230866778&fm=193&f=GIF',
@@ -31,28 +34,100 @@ const data = [
 ];
 
 class Carousel extends React.Component {
+  state = {
+    activeSlide: 0,
+  };
+
+  onSnapToItem = (index: number) => {
+    this.setState({
+      activeSlide: index,
+    });
+  };
+
   renderItem = (
     {item}: {item: string},
+    parallaxProps?: AdditionalParallaxProps,
   ) => {
-    return <Image source={{uri: item}} style={styles.image} />;
+    return (
+      <ParallaxImage
+        source={{uri: item}}
+        containerStyle={styles.imageContainer}
+        style={styles.image}
+        parallaxFactor={0.35}
+        showSpinner={true}
+        spinnerColor="rgba(0, 0, 0, 0.25)"
+        {...parallaxProps}
+      />
+    );
   };
+
+  get pagination() {
+    const {activeSlide} = this.state;
+    return (
+      <View style={styles.paginationWrapper}>
+        <Pagination
+          dotsLength={data.length}
+          activeDotIndex={activeSlide}
+          containerStyle={styles.paginationContainer}
+          dotContainerStyle={styles.dotContainer}
+          dotStyle={styles.dot}
+          dotColor={'rgba(255, 255, 255, 0.92)'}
+          inactiveDotScale={0.7}
+          inactiveDotOpacity={0.4}
+        />
+      </View>
+    );
+  }
 
   render() {
     return (
-      <SnapCarousel
-        data={data}
-        renderItem={this.renderItem}
-        sliderWidth={sliderWidth}
-        itemWidth={itemWidth}
-      />
+      <View>
+        <SnapCarousel
+          data={data}
+          renderItem={this.renderItem}
+          sliderWidth={sliderWidth}
+          itemWidth={itemWidth}
+          hasParallaxImages
+          onSnapToItem={this.onSnapToItem}
+          loop
+          autoplay
+        />
+        {this.pagination}
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  image: {
+  imageContainer: {
     width: itemWidth,
-    height: sideHeight,
+    height: slideHeight,
+    borderRadius: 8,
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    resizeMode: 'cover',
+  },
+  paginationWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  paginationContainer: {
+    position: 'absolute',
+    top: -30,
+    backgroundColor: 'rgba(0, 0, 0, 0.35)',
+    paddingHorizontal: 3,
+    paddingVertical: 4,
+    borderRadius: 8,
+  },
+  dotContainer: {
+    marginHorizontal: 6,
+  },
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: 'rgba(255, 255, 255, 0.92)',
   },
 });
 
